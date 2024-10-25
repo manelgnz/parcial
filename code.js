72e0969c37b89e7cfdb70187b3fc2f788f46b57b
manelgnz <ggooonzz@gmail.com>
2024-10-25 09:29:03 +0200

    commit4

--- code.js
+++ code.js
@@ -2,9 +2,23 @@ const URL = 'https://pokeapi.co/api/v2/';
 window.onload = async function(){
     const pokemons = await getAllPokemons();
     const list = document.getElementById('pokemon-list');
-    for (pokemon of pokemons){
+    for (let pokemon of pokemons){
         const pokemonItem = document.createElement('li');
-        pokemonItem.innerText = pokemon.name; 
+        pokemonItem.innerText = pokemon.name;
+        
+        //onclick
+        pokemonItem.onclick = async function() {
+            const position = getPositionPokemon(pokemon.name, pokemons);
+            let positionSpan = pokemonItem.querySelector('.position-span');
+            if (!positionSpan) {
+                positionSpan = document.createElement('span');
+                positionSpan.className = 'position-span';
+                positionSpan.style.display = 'block'; 
+                this.appendChild(positionSpan);
+            }
+            positionSpan.innerText = `ID: ${position}`;
+        };
+
         list.appendChild(pokemonItem);
     }
 }
@@ -15,11 +29,11 @@ async function getAllPokemons(){
     const pokemonList = data.results;
     return pokemonList;
 }
-async function getPositionPokemon(name){
-    const pokemons = await getAllPokemons();
+function getPositionPokemon(name, pokemons){
     for (let i = 0; i < pokemons.length; i++){
         if (pokemons[i].name === name){
             return i;
         }
     }
+    
 }
