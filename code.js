f5ab99afa803bdd6e647bdd540c514e0b56f3f28
manelgnz <ggooonzz@gmail.com>
2024-10-25 10:44:02 +0200

    commit11

--- code.js
+++ code.js
@@ -6,6 +6,13 @@ window.onload = async function(){
         const pokemonItem = document.createElement('li');
         pokemonItem.innerText = pokemon.name;
         
+        //Boto afegir al equip
+        const addButton = document.createElement('button');
+        addButton.innerText = 'Afegir al equip';
+        addButton.onclick = function(){
+            afegirAlEquip(pokemon);
+        };
+        pokemonItem.appendChild(addButton);
         //onclick
         pokemonItem.onclick = async function() {
             const ID = getPositionPokemon(pokemon.name, pokemons);
@@ -97,14 +104,22 @@ async function searchPokemon(name){
     }
 
 }
-async function searchPokemonByName(){
+async function searchPokemonByName(event){
+    event.preventDefault(); // evito que el formulari s'envii
     const input = document.getElementById('pokemon-input');
     const busqueda = document.getElementById('busqueda');
     const pokemonName = input.value;
     const pokemon = await searchPokemon(pokemonName);
     if (pokemon){ //si existeix
-        busqueda.innerHTML = ''; // Limpiar resultados anteriores
-        busqueda.innerText = `ID: ${pokemon.id} Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}`;
+        busqueda.innerHTML = ''; // netejo contingut anterior busca't
+        let tipos = '';
+        for (let i = 0; i < pokemon.types.length; i++) {
+            tipos += pokemon.types[i].type.name;
+            if (i < pokemon.types.length - 1) {
+                tipos += ' ';
+            }
+        }
+        busqueda.innerText = `ID: ${pokemon.id} Type: ${tipos}`;
         const img = document.createElement('img');
         img.className = 'imagen-search';
         img.src = pokemon.sprites.other['official-artwork'].front_default;
@@ -113,4 +128,22 @@ async function searchPokemonByName(){
         alert('Pokemon no trobat');
     }
     return busqueda.innerText;
+}
+function afegirAlEquip(pokemon) {
+    if (equipo.length > 5) {
+        alert('Equip ple');
+        return;
+    } //sino, afegim el pokemon a l'equip
+    equipo.push(pokemon);
+    updateEquipList();
+}
+
+function updateEquipList() {
+    const equipList = document.getElementById('equip-list');
+    equipList.innerHTML = ''; // Limpiar la lista anterior
+    for (let pokemon of equip) {
+        const pokemonItem = document.createElement('li');
+        pokemonItem.innerText = pokemon.name;
+        equipList.appendChild(pokemonItem);
+    }
 }
